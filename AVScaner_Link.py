8e4d6ca5aa98800203497ae8ff96787f3787a5d6
sky <email@email.com>
2024-09-15 01:35:50 +0300

    var 2

--- AVScaner_Link.py
+++ AVScaner_Link.py
@@ -2,15 +2,11 @@ import asyncio
 import random
 from dataclasses import dataclass
 from typing import Optional
-from asyncio import Queue
-import aiohttp
-import re
-from handlers.file_handler import read_file_to_list, load_patterns, read_file_to_queue
-from handlers.parse_arguments import parse_arguments
-from handlers.utils import C, timer_decorator, limit_rate_decorator
-from aiohttp import ClientConnectorCertificateError, ClientSSLError
-from aiohttp import ClientSession
 import aiofiles
+import re
+
+from handlers.parse_arguments import parse_arguments
+from handlers.utils import C
 
 PARSE_ARGS = parse_arguments()
 
@@ -24,26 +20,22 @@ VERBOSE = None
 URL_ENCODE = None
 PROXY = PARSE_ARGS.proxy
 
-
-
-
 @dataclass
 class Task:
-    tid: str # tid = task_id
+    url: str
 
     async def perform(self, pool):
-        print(f'{C.blue}[+] ==> start perform: {self.tid}{C.norm}')
-        # _time = random.randint(1, 5)
+        print(f'{C.blue}[+] ==> start perform: {self.url}{C.norm}')
         _time = 2
         await asyncio.sleep(_time)
-        print(f'{C.green}[-] <== complete perform: {self.tid}, _time={_time}{C.norm}')
+        print(f'{C.green}[-] <== complete perform: {self.url}, _time={_time}{C.norm}')
 
 
 class Pool:
     def __init__(self, max_rate: int, interval: int = 1, concurrent_level: Optional[int] = None):
-        self.max_rate = max_rate  # максимальное количество запросов
-        self.interval = interval  # если мы передаем значения max_rate = 5 и interval = 1, в секунду может исполняться 5 запросов
-        self.concurrent_level = concurrent_level  # обозначает допустимое количество параллельных запросов
+        self.max_rate = max_rate
+        self.interval = interval
+        self.concurrent_level = concurrent_level
         self.is_running = False
         self._queue = asyncio.Queue(maxsize=100)
         self._urls_with_payload_queue = asyncio.Queue(maxsize=100)
@@ -55,34 +47,36 @@ class Pool:
         self.content_list = []
         self.combined_pattern = None
 
-
     async def _worker(self, task: Task):
-        async with self._sem:  # Мы занимаем наш Semaphore пока операции внутри кода не закончатся
+        async with self._sem:
             self._cuncurrent_workers += 1
-
             await task.perform(self)
-
             self._urls_with_payload_queue.task_done()  # это означает, что задачу мы выполнили
         self._cuncurrent_workers -= 1
         if not self.is_running and self._cuncurrent_workers == 0:
             self._stop_event.set()
 
-
     async def _scheduler(self):
         while self.is_running:
-
             link = await self._queue.get()
-            await self.generate_payload_urls(link, self.content_list)
+            scheme = link.replace('https://', 'http://')
+            base_url = scheme.split('=')[0]
+
+            # Для каждого payload генерируем URL и добавляем в очередь
+            for payload in self.content_list:
+                await self._urls_with_payload_queue.put(f"{base_url}={payload}")
+
+            print(f'{C.blue}[*] Generated URLs with payloads added to queue.{C.norm}')
+
+            # await self.generate_payload_urls(link, self.content_list)
 
             for _ in range(self.max_rate):
-                async with self._sem:  # И чтоб не сыпать задачами Semaphore добавим сюда
-                    task = await self._urls_with_payload_queue.get()
-                    # print(task)
-                    asyncio.create_task(self._worker(Task(task)))
+                async with self._sem:
+                    url = await self._urls_with_payload_queue.get()
+                    asyncio.create_task(self._worker(Task(url)))
 
             await asyncio.sleep(self.interval)
             self._queue.task_done()
-
 
     def start(self):
         self.is_running = True
@@ -94,71 +88,55 @@ class Pool:
     async def join(self):
         await self._queue.join()
 
+    async def join_urls_with_payload(self):
+        await self._urls_with_payload_queue.join()
+
     async def stop(self):
         self.is_running = False
         self._scheduler_task.cancel()
         if self._cuncurrent_workers != 0:
             await self._stop_event.wait()
 
-# -----------------------------------------------------------
     async def put_urls_with_payload(self, task):
         await self._urls_with_payload_queue.put(task)
 
-    async def join_urls_with_payload(self):
-        await self._urls_with_payload_queue.join()
 
-    async def generate_payload_urls(self, link: str, payload_patterns: list[str]):
-        scheme = link.replace('https://', 'http://')
-        base_url = scheme.split('=')[0]
+    # async def generate_payload_urls(self, link: str, payload_patterns: list[str]):
+    #     scheme = link.replace('https://', 'http://')
+    #     base_url = scheme.split('=')[0]
+    #
+    #     for payload in payload_patterns:
+    #         await self._urls_with_payload_queue.put(f"{base_url}={payload}")
 
-        for payload in payload_patterns:
-            # print(f"{base_url}={payload}")
-            await self._urls_with_payload_queue.put(f"{base_url}={payload}")
-            # print(f'Producer put  to the _urls_with_payload_queue, {self._urls_with_payload_queue}')
-
-
-    async def read_file_to_queue(self, file_path: str):  # producer
+    async def read_file_to_queue(self, file_path: str):
         async with aiofiles.open(file_path, mode='r') as file:
             async for line in file:
                 await self._queue.put(line.strip())
-                # print(f'Producer put  to the queue, {self._queue}')
-
 
     async def read_file_to_list(self, file_path: str):
         async with aiofiles.open(file_path, mode='r') as file:
             async for line in file:
                 self.content_list.append(line.strip())
-                if len(self.content_list) % 1000 == 0:
-                    await asyncio.sleep(0)
-        print(
-            f'{C.yellow}[*] Total number of payload variants per link: {C.bold_yellow}{len(self.content_list )}\n\n{C.norm}')
-        # return self.content_list
-
+        print(f'{C.yellow}[*] Total number of payload variants per link: {C.bold_yellow}{len(self.content_list)}\n\n{C.norm}')
 
     async def load_patterns(self, file_path):
         async with aiofiles.open(file_path, mode='r') as file:
             patterns = [line.strip() for line in await file.readlines()]
 
         self.combined_pattern = re.compile('|'.join(re.escape(pattern) for pattern in patterns))
-        # return combined_pattern
-
 
 
 async def start(pool):
-
     await pool.read_file_to_queue(INPUT)
     await pool.read_file_to_list(PAYLOADS)
     await pool.load_patterns(ANSWERS)
 
-    # print(pool.content_list)
-    # print(pool.combined_pattern)
-
-
+    # print(pool._urls_with_payload_queue)
 
     pool.start()
-    await pool.join()  # краулер будет ждать до тех пор, пока очередь не опустеет async def join
+    await pool.join()
     await pool.join_urls_with_payload()
-    await pool.stop()  # после того, как внутри пула закончатся задачи, его нужно корректно остановить
+    await pool.stop()
 
 
 def main():
@@ -169,11 +147,10 @@ def main():
     pool = Pool(CALL_LIMIT_PER_SECOND)
 
     try:
-        # Запускаем корутину до завершения
         loop.run_until_complete(start(pool))
     except KeyboardInterrupt:
         loop.run_until_complete(pool.stop())
-        # Закрываем цикл событий при прерывании
+    finally:
         loop.close()
 
 
