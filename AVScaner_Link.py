4a79cdd2e0f65bcc18d38e4312723fad83e087d9
sky <email@email.com>
2024-09-15 12:23:46 +0300

    var 3 working version

--- AVScaner_Link.py
+++ AVScaner_Link.py
@@ -25,9 +25,9 @@ class Task:
     url: str
 
     async def perform(self, pool):
-        _time = random.randint(1, 5)
+        # _time = random.randint(3, 5)
+        _time = 1
         print(f'{C.blue}[+] ==> start perform: {self.url}, _time={_time}{C.norm}')
-
         await asyncio.sleep(_time)
         print(f'{C.green}   [-] <== complete perform: {self.url}, _time={_time}{C.norm}')
 
@@ -38,84 +38,80 @@ class Pool:
         self.interval = interval
         self.concurrent_level = concurrent_level
         self.is_running = False
-        self._queue = asyncio.Queue(maxsize=100)
-        self._urls_with_payload_queue = asyncio.Queue(maxsize=100)
-        self._scheduler_task: Optional[asyncio.Task] = None
-        self._scheduler_task_1: Optional[asyncio.Task] = None
+        self._link_queue = asyncio.Queue(maxsize=100)
+        self._payloadUrls_queue = asyncio.Queue(maxsize=100)
+        self._scheduler_task_link_queue: Optional[asyncio.Task] = None
+        self._scheduler_task_payloadUrls_queue: Optional[asyncio.Task] = None
         self._sem = asyncio.Semaphore(concurrent_level or max_rate)
         self._cuncurrent_workers = 0
         self._stop_event = asyncio.Event()
 
-        self.content_list = []
+        self.payloads = []
         self.combined_pattern = None
 
     async def _worker(self, task: Task):
         async with self._sem:
             self._cuncurrent_workers += 1
+
             await task.perform(self)
-            self._urls_with_payload_queue.task_done()  # это означает, что задачу мы выполнили
+
+            self._payloadUrls_queue.task_done()  # это означает, что задачу мы выполнили
         self._cuncurrent_workers -= 1
         if not self.is_running and self._cuncurrent_workers == 0:
             self._stop_event.set()
 
-    async def _scheduler(self):
+    async def _scheduler_payloadUrls_queue(self):
         while self.is_running:
             for _ in range(self.max_rate):
                 async with self._sem:
-                    url = await self._urls_with_payload_queue.get()
-                    # print(url)
+                    url = await self._payloadUrls_queue.get()
                     asyncio.create_task(self._worker(Task(url)))
             await asyncio.sleep(self.interval)
 
-    async def _scheduler_1(self):
+    async def _scheduler_link_queue(self):
         while self.is_running:
-            link = await self._queue.get()
-            await self.generate_payload_urls(link, self.content_list)
-            self._queue.task_done()
+            link = await self._link_queue.get()
+            await self.generate_payload_urls(link, self.payloads)
+            self._link_queue.task_done()
 
     def start(self):
         self.is_running = True
-        self._scheduler_task_1 = asyncio.create_task(self._scheduler_1())
-        self._scheduler_task = asyncio.create_task(self._scheduler())
-
-    async def put(self, task):
-        await self._queue.put(task)
+        self._scheduler_task_link_queue = asyncio.create_task(self._scheduler_link_queue())
+        self._scheduler_task_payloadUrls_queue = asyncio.create_task(self._scheduler_payloadUrls_queue())
 
     async def join(self):
-        await self._queue.join()
+        await self._link_queue.join()
 
     async def join_urls_with_payload(self):
-        await self._urls_with_payload_queue.join()
+        await self._payloadUrls_queue.join()
 
     async def stop(self):
         self.is_running = False
-        self._scheduler_task_1.cancel()
-        self._scheduler_task.cancel()
+        self._scheduler_task_link_queue.cancel()
+        self._scheduler_task_payloadUrls_queue.cancel()
         if self._cuncurrent_workers != 0:
             await self._stop_event.wait()
 
     async def put_urls_with_payload(self, task):
-        await self._urls_with_payload_queue.put(task)
+        await self._payloadUrls_queue.put(task)
 
     async def generate_payload_urls(self, link: str, payload_patterns: list[str]):
         scheme = link.replace('https://', 'http://')
         base_url = scheme.split('=')[0]
 
         for payload in payload_patterns:
-            await self._urls_with_payload_queue.put(f"{base_url}={payload}")
-            # print(f"{self._urls_with_payload_queue.qsize()} {payload}")
-        # print(f"{self._urls_with_payload_queue}")
+            await self._payloadUrls_queue.put(f"{base_url}={payload}")
 
     async def read_file_to_queue(self, file_path: str):
         async with aiofiles.open(file_path, mode='r') as file:
             async for line in file:
-                await self._queue.put(line.strip())
+                await self._link_queue.put(line.strip())
 
     async def read_file_to_list(self, file_path: str):
         async with aiofiles.open(file_path, mode='r') as file:
             async for line in file:
-                self.content_list.append(line.strip())
-        print(f'{C.yellow}[*] Total number of payload variants per link: {C.bold_yellow}{len(self.content_list)}\n\n{C.norm}')
+                self.payloads.append(line.strip())
+        print(f'{C.yellow}[*] Total number of payload variants per link: {C.bold_yellow}{len(self.payloads)}\n\n{C.norm}')
 
     async def load_patterns(self, file_path):
         async with aiofiles.open(file_path, mode='r') as file:
@@ -129,8 +125,8 @@ async def start(pool):
     await pool.read_file_to_list(PAYLOADS)
     await pool.load_patterns(ANSWERS)
 
-
     pool.start()
+
     await pool.join()
     await pool.join_urls_with_payload()
     await pool.stop()
